39e115c7720211fb5d499249a5d8eb42e934d0c1
junyoung.chung <junyoung.chung@navercorp.com>
2018-10-31 23:40:54 +0900

    chapter03

--- chapter03/seneca-services/index.js
+++ chapter03/seneca-services/index.js
@@ -3,4 +3,5 @@ var seneca = require("seneca")()
 			.use("sms")
 			.use("post");
 
-seneca.listen({port: 1932, host: "10.0.0.7"});
+//seneca.listen({port: 1932, host: "10.0.0.7"});
+seneca.listen({port: 1932, host: "127.0.0.1"});
--- chapter03/seneca-services/index2.js
+++ chapter03/seneca-services/index2.js
@@ -2,10 +2,11 @@ var seneca = require("seneca")()
 			.use("email")
 			.use("sms")
 			.use("post");
-seneca.listen({port: 1932, host: "10.0.0.7"});
+//seneca.listen({port: 1932, host: "10.0.0.7"});
+seneca.listen({port: 1932, host: "127.0.0.1"});
 
 // interact with the existing email service using "seneca"
 
-var senecaEmail = require("seneca").client({host: "new-email-service-ip", port: 1932});
+var senecaEmail = require("seneca")().listen({host: "10.25.209.39", port: 1932});
 
 // interact with the new email service using "senecaEmail"
